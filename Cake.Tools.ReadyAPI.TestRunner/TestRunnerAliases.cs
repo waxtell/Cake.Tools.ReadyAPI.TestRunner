258a33521caf80c8786cf5e2c4b85263e3cb62c1
Will Axtell <waxtell@transtar1.com>
2021-01-08 13:36:14 -0500

    Initial commit

---
+++ Cake.Tools.ReadyAPI.TestRunner/TestRunnerAliases.cs
@@ -0,0 +1,62 @@
+using Cake.Core;
+using Cake.Core.Annotations;
+
+namespace Cake.Tools.ReadyAPI.TestRunner
+{
+    /// <summary>
+    /// <para>
+    /// <code>#addin Cake.Tools.ReadyAPI.TestRunner</code>
+    /// </para>
+    /// <para>
+    /// If TestRunner.bat is not included in the PATH you will have to provide the location of TestRunner.bat as such
+    /// <example>
+    /// <code>
+    /// Setup(context => {
+    ///     context.Tools.RegisterFile("C:/Program Files/SmartBear/ReadyAPI-3.5.1/bin/testrunner.bat");
+    /// });
+    /// </code>
+    /// </example>
+    /// </para>
+    /// </summary>
+    [CakeAliasCategory("ReadyAPITestRunner")]
+    public static class TestRunnerAliases
+    {
+        /// <summary>
+        /// Run your ReadyAPI functional test(s).
+        /// <example>
+        /// <code>
+        /// var result = LaunchTestRunner
+        /// (
+        ///     "your-readyapi-project.xml",
+        ///     new TestRunnerSettings()
+        ///     {
+        ///         EndPoint = "http://localhost",
+        ///         PrintSummaryReport = true
+        ///     }
+        /// );
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="context">The Cake context.</param>
+        /// <param name="projectFile">The ReadyAPI project.</param>
+        /// <param name="settings">The settings that will be provided to TestRunner.bat.</param>
+        /// <returns>0 for success.  Please see <a href="https://support.smartbear.com/readyapi/docs/functional/running/automating/exit-codes.html">Exit Codes</a> for additional information</returns>
+        [CakeMethodAlias]
+        public static int LaunchTestRunner(this ICakeContext context, string projectFile, TestRunnerSettings settings)
+        {
+            return
+                new TestRunnerRunner
+                (
+                    context.FileSystem,
+                    context.Environment,
+                    context.ProcessRunner,
+                    context.Tools
+                )
+                .RunFunctionalTests
+                (
+                    projectFile, 
+                    settings
+                );
+        }
+    }
+}
---
+++ Cake.Tools.ReadyAPI.TestRunner/TestRunnerRunner.cs
@@ -0,0 +1,238 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.Tools.ReadyAPI.TestRunner
+{
+    internal sealed class TestRunnerRunner : Tool<TestRunnerSettings>
+    {
+        public TestRunnerRunner(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner, IToolLocator tools)
+            : base(fileSystem, environment, processRunner, tools)
+        {
+        }
+
+        public int RunFunctionalTests(string projectFile, TestRunnerSettings settings)
+        {
+            if (string.IsNullOrWhiteSpace(projectFile))
+            {
+                throw new ArgumentNullException(nameof(projectFile));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            using (var p = RunProcess(settings, GetArguments(projectFile, settings)))
+            {
+                p.WaitForExit();
+
+                return p.GetExitCode();
+            }
+        }
+
+        protected override IEnumerable<string> GetToolExecutableNames()
+        {
+            return new[] { "testrunner", "testrunner.bat" };
+        }
+
+        protected override string GetToolName()
+        {
+            return "testrunner";
+        }
+
+        private static ProcessArgumentBuilder GetArguments(string projectFile, TestRunnerSettings settings)
+        {
+            var arguments = new ProcessArgumentBuilder();
+
+            arguments.Append(projectFile);
+
+            if (settings.ExportResults)
+            {
+                arguments.Append("-a");
+            }
+            
+            if (settings.ExportResultsUsingFolders)
+            {
+                arguments.Append("-A");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.TestCase))
+            {
+                arguments.Append($"-c{settings.TestCase}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.Domain))
+            {
+                arguments.Append($"-d{settings.Domain}");
+            }
+
+            if (settings.SystemProperties != null)
+            {
+                foreach (var arg in settings.SystemProperties)
+                {
+                    arguments.Append($"-D{arg.Key}=\"{arg.Value}\"");
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.EndPoint))
+            {
+                arguments.Append($"-e{settings.EndPoint}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.Environment))
+            {
+                arguments.Append($"-E{settings.Environment}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.OutputFolder))
+            {
+                arguments.Append($"-f{settings.OutputFolder}");
+            }
+
+            if (settings.ReportFormats != null)
+            {
+                arguments.Append($"-F{string.Join(",", settings.ReportFormats)}");
+            }
+
+            if (settings.CreateCoverageReport)
+            {
+                arguments.Append("-g");
+            }
+
+            if (settings.GlobalProperties != null)
+            {
+                foreach (var arg in settings.GlobalProperties)
+                {
+                    arguments.Append($"-G{arg.Key}=\"{arg.Value}\"");
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.Host))
+            {
+                arguments.Append($"-h{settings.Host}");
+            }
+
+            if (settings.Headers != null)
+            {
+                foreach (var header in settings.Headers)
+                {
+                    arguments.Append($"-H{header.Key}={header.Value}");
+                }
+            }
+
+            if (settings.EnableUIComponents)
+            {
+                arguments.Append("-i");
+            }
+
+            if (settings.IgnoreErrors)
+            {
+                arguments.Append("-I");
+            }
+
+            if (settings.CreateJUnitCompatibleReports)
+            {
+                arguments.Append("-j");
+            }
+
+            if (settings.IncludeTestProperties)
+            {
+                arguments.Append("-J");
+            }
+
+            if (settings.BriefResults)
+            {
+                arguments.Append("-M");
+            }
+
+            if (settings.OpenReportsInBrowser)
+            {
+                arguments.Append("-o");
+            }
+
+            if (settings.SuppressUsageStatistics)
+            {
+                arguments.Append("-O");
+            }
+
+            if (settings.ProjectProperties != null)
+            {
+                foreach (var arg in settings.ProjectProperties)
+                {
+                    arguments.Append($"-P{arg.Key}=\"{arg.Value}\"");
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.Password))
+            {
+                arguments.Append($"-p{settings.Password}");
+            }
+
+            if (settings.PrintSummaryReport)
+            {
+                arguments.Append("-r");
+            }
+            
+            if (!string.IsNullOrWhiteSpace(settings.Report))
+            {
+                arguments.Append($"\"-R{settings.Report}\"");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.TestSuite))
+            {
+                arguments.Append($"-s{settings.TestSuite}");
+            }
+
+            if (settings.SaveProject)
+            {
+                arguments.Append("-S");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.SettingsFile))
+            {
+                arguments.Append($"-t{settings.SettingsFile}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.TestSuiteTags))
+            {
+                arguments.Append($"\"-TTestSuite {settings.TestSuiteTags}\"");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.TestCaseTags))
+            {
+                arguments.Append($"\"-TTestCase {settings.TestCaseTags}\"");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.Username))
+            {
+                arguments.Append($"-u{settings.Username}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.Password))
+            {
+                arguments.Append($"-v{settings.Password}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.PasswordType))
+            {
+                arguments.Append($"-w{settings.PasswordType}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.SlackAccessToken) && settings.SlackChannels != null && settings.SlackChannels.Any())
+            {
+                arguments.Append($"-W{settings.SlackAccessToken}/{string.Join(",", settings.SlackChannels)}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(settings.ProjectPassword))
+            {
+                arguments.Append($"-x{settings.ProjectPassword}");
+            }
+
+            return arguments;
+        }
+    }
+}
